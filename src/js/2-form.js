73aa891029c4a4cfc1d51e549202b09bf7f53b19
Oleksandr Kuzmenko <alexaniyyy@gmail.com>
2025-03-01 23:07:18 +0000

    ch

---
+++ src/js/2-form.js
@@ -0,0 +1,36 @@
+const form = document.querySelector(".feedback-form");
+const input = document.querySelector("input");
+const textarea = document.querySelector("textarea");
+const localStorageKey = "feedback-form-state";
+
+const formData = {
+    email: "",
+    message: "",
+}
+
+const saveData = localStorage.getItem(localStorageKey);
+if(saveData) {
+try{
+    const parsedData = JSON.parse(savedData);
+            if (parsedData.email) emailInput.value = parsedData.email;
+            if (parsedData.message) messageInput.value = parsedData.message;
+            formData.email = parsedData.email 
+            formData.message = parsedData.message  
+        } catch (error) {
+            console.error("Error parsing data:", error);
+}
+}
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    if(!formData.email || formData.message){
+        alert("Fill all fiends");
+        return
+    }
+console.log("Form Data Submitted", formData);
+localStorage.removeItem(localStorageKey);
+form.reset();
+formData.email = "";
+formData.message = "";
+
+});
