c03af75cd58cd6529d029976a2f7706fbd4ba15c
Oleksandr Kuzmenko <alexaniyyy@gmail.com>
2025-03-03 16:12:16 +0000

    ch

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,5 +1,7 @@
+
 import SimpleLightbox from "simplelightbox";
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm"
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 const images = [
   {
     preview:
