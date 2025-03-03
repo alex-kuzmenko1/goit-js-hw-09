bfc955ae3f18cbc411f74d0c53c43c179b2e0fef
Oleksandr Kuzmenko <alexaniyyy@gmail.com>
2025-03-03 18:16:48 +0000

    ch

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,7 +1,4 @@
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
 const images = [
   {
     preview:
@@ -68,30 +65,35 @@ const images = [
   },
 ];
 
-const galeryList = document.querySelector('.gallery');
-
-const markup = images
-  .map(
-    ({ preview, original, description }) => `
-      <li class="gallery-item">
-        <a class="gallery-link" href="${original}">
-          <img
-            class="gallery-image"
-            src="${preview}"
-            alt="${description}"
-          />
-        </a>
-      </li>
-    `
-  )
-  .join('');
-
-galeryList.innerHTML = markup;
-
-const lightbox = new SimpleLightbox('.gallery a', {
+new SimpleLightbox(".gallery a", {
   captions: true,
-  captionSelector: 'img',
-  captionsData: 'alt',
+  captionSelector: "img",
+  captionsData: "alt",
   captionDelay: 250,
 });
-const captionSelector = captionSelector;
+const galleryList = document.querySelector(".gallery");
+
+if (galleryList) {
+  const markup = images
+    .map(
+      ({ preview, original, description }) => `
+        <li class="gallery-item">
+          <a class="gallery-link" href="${original}">
+            <img class="gallery-image" src="${preview}" alt="${description}" />
+          </a>
+        </li>
+      `
+    )
+    .join("");
+
+  galleryList.innerHTML = markup;
+
+  new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionSelector: "img",
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+} else {
+  console.error("Элемент .gallery не найден в HTML!");
+}
