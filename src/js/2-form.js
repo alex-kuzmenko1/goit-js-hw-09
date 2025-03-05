ac1b808aa802076c70b323cfba22e07cc3b6fb8e
Oleksandr Kuzmenko <alexaniyyy@gmail.com>
2025-03-05 19:56:29 +0000

    ch

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,40 +1,44 @@
 const form = document.querySelector(".feedback-form");
-const input = document.querySelector("input");
-const textarea = document.querySelector("textarea");
+const emailInput = form.querySelector("input");
+const messageInput = form.querySelector("textarea");
 const localStorageKey = "feedback-form-state";
 
-const formData = {
+let formData = {
     email: "",
     message: "",
+};
+
+
+const savedData = localStorage.getItem(localStorageKey);
+if (savedData) {
+    try {
+        const parsedData = JSON.parse(savedData);
+        formData = { 
+            email: parsedData.email || "", 
+            message: parsedData.message || "" 
+        };
+        emailInput.value = formData.email;
+        messageInput.value = formData.message;
+    } catch (error) {
+        console.error("Error parsing data:", error);
+    }
 }
 
-const saveData = localStorage.getItem(localStorageKey);
-if(saveData) {
-try{
-    const parsedData = JSON.parse(savedData);
-            if (parsedData.email) emailInput.value = parsedData.email;
-            if (parsedData.message) messageInput.value = parsedData.message;
-            formData.email = parsedData.email 
-            formData.message = parsedData.message  
-        } catch (error) {
-            console.error("Error parsing data:", error);
-}
-}
 form.addEventListener("input", (event) => {
     formData[event.target.name] = event.target.value;
     localStorage.setItem(localStorageKey, JSON.stringify(formData));
 });
-
-form.addEventListener("submit", (event) =>{
+form.addEventListener("submit", (event) => {
     event.preventDefault();
-    if(!formData.email || formData.message){
-        alert("Fill all fields.");
-        return
-    }
-console.log("Form Data Submitted", formData);
-localStorage.removeItem(localStorageKey);
-form.reset();
-formData.email = "";
-formData.message = "";
 
+    if (!formData.email || !formData.message) { 
+        alert("Fill all fields.");
+        return;
+    }
+
+    console.log("Form Data Submitted:", formData);
+
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+    formData = { email: "", message: "" }; 
 });
